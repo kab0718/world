0f54dbeeef0bd9e6a344879e78d7d738fc2a76f3
K-Pompei <15065@hakodate.kosen-ac.jp>
2019-11-07 09:33:29 +0900

    サブクラスを追加

--- world/MyWorld.java
+++ world/MyWorld.java
@@ -17,6 +17,6 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 800, 1); 
-        addObject( new kab(), 100,200 );
+        addObject( new cab(), 100,200 );
     }
 }
---
+++ world/Pompei.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pompei here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pompei extends Actor
+{
+    /**
+     * Act - do whatever the Pompei wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
