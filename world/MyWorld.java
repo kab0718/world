49004bbe3124f6a3aea91a4ac84c7e370fc5468e
kab0718 <kab3nan9@gmail.com>
2020-01-30 10:28:13 +0900

    BGM追加

--- world/MyWorld.java
+++ world/MyWorld.java
@@ -13,6 +13,7 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    static GreenfootSound sound = new GreenfootSound("sounds/bgm.mp3" );
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -20,6 +21,7 @@ public class MyWorld extends World
         addObject( new cab(), 500,150 );
         addObject( new Pompei(), 500,700 );
         //addObject( new Tweet(), 500,500 );
+        sound.play();
         for(int i=20;i<641;i+=32){
             addObject( new hato(20-(i-20)/32), i,16 );        
         } 
